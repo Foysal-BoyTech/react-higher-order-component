bcc0ddd236b1fa82d7034b78e2a75f902ab0f863
Foysal Mohammad <foysal@earlblue.com>
2021-06-20 15:31:37 +0100

    added useReducer in counter one and it works

---
+++ src/components/Hooks/CounterOne.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from 'react'
+const initialVal = 0
+const reducer = (state, action) => {
+  // eslint-disable-next-line default-case
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialVal
+    default:
+      return state
+  }
+}
+
+export default function CounterOne() {
+  const [count, dispatch] = useReducer(reducer, initialVal)
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch('decrement')}>Decrement</button>
+      <button onClick={() => dispatch('reset')}>Reset</button>
+      <button onClick={() => dispatch('increment')}>Increment</button>
+    </div>
+  )
+}
