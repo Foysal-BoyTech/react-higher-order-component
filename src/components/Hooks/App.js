aadfc95896668d29f4ad4e47786e91fe312d32f6
Foysal Mohammad <foysal@earlblue.com>
2021-06-21 22:55:49 +0100

    used useMemo hook in app component

--- src/components/Hooks/App.js
+++ src/components/Hooks/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useMemo } from 'react'
 import axios from 'axios'
 import DataFetching2 from './DataFetching2'
 import ParentComponent from './ParentComponent'
@@ -9,6 +9,22 @@ import ParentComponent from './ParentComponent'
 // export const ChannelContext = React.createContext()
 
 export default function App() {
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+  const increment1 = () => {
+    setCount1(count1 + 1)
+  }
+
+  const increment2 = () => {
+    setCount2(count2 + 1)
+  }
+
+  const isEven = useMemo(() => {
+    let i = 0
+    while (i < 1000000000) i++
+    return count1 % 2 === 0
+  }, [count1])
   return (
     <div>
       {/* <UserContext.Provider value={'Foysal'}>
@@ -16,6 +32,11 @@ export default function App() {
           <ComponentC />
         </ChannelContext.Provider>
       </UserContext.Provider> */}
+      <div>
+        <button onClick={increment1}>count1 - {count1} </button>
+        <span>{isEven ? 'Even' : 'Odd'}</span>
+      </div>
+      <button onClick={increment2}>count2 - {count2} </button>
       <ParentComponent />
     </div>
   )
